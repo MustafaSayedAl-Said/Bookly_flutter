77dc143404517fb1c11738c450076092492fd4d6
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2023-08-27 21:04:37 +0300

    Add custom list view item to home

---
+++ lib/features/home/presentation/views/widgets/custom_list_view_item.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+
+import '../../../../../core/utils/assets.dart';
+
+
+class CustomListViewItem extends StatelessWidget {
+  const CustomListViewItem({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * .23,
+      child: AspectRatio(
+        aspectRatio: 2.7 / 4,
+        child: Container(
+          //height:  MediaQuery.of(context).size.height * .3,
+          //width: MediaQuery.of(context).size.width * .25,
+          decoration: BoxDecoration(
+              color: Colors.red,
+              borderRadius: BorderRadius.circular(16),
+              image: const DecorationImage(
+                fit: BoxFit.fill,
+                image: AssetImage(AssetsData.testImage),
+              )),
+        ),
+      ),
+    );
+  }
+}
