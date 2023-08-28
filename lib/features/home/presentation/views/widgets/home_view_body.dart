751922350d4e6490db657ad1912c4bd5017cf0b2
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2023-08-28 21:59:24 +0300

    best seller list view item part one

--- lib/features/home/presentation/views/widgets/home_view_body.dart
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookley/core/utils/assets.dart';
 import 'package:my_bookley/core/utils/styles.dart';
 import 'custom_app_bar.dart';
 import 'featured_list_view.dart';
@@ -21,9 +22,41 @@ class HomeViewBody extends StatelessWidget {
           Text(
             'Best Seller',
             style: Styles.titleMedium,
-          )
+          ),
+          BestSellerListItem(),
         ],
       ),
     );
   }
 }
+
+class BestSellerListItem extends StatelessWidget {
+  const BestSellerListItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 125,
+      child: Row(
+        children: [AspectRatio(
+          aspectRatio: 2.5 / 4,
+          child: Container(
+            //height:  MediaQuery.of(context).size.height * .3,
+            //width: MediaQuery.of(context).size.width * .25,
+            decoration: BoxDecoration(
+                color: Colors.red,
+                borderRadius: BorderRadius.circular(8),
+                image: const DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage(AssetsData.testImage),
+                )),
+          ),
+        ),
+        const Column(
+
+        )],
+      ),
+    );
+  }
+}
+
