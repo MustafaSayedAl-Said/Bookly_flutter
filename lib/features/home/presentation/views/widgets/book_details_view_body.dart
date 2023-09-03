c4130151babbfb09f57849a23e85017b0c57ed08
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2023-09-03 22:27:40 +0300

    Added title and subtitle to details

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:my_bookley/core/utils/styles.dart';
 import 'package:my_bookley/features/home/presentation/views/widgets/custom_book_item.dart';
 
 import 'custom_book_details_appbar.dart';
@@ -14,12 +15,29 @@ class BookDetailsViewBody extends StatelessWidget {
         children: [
           const CustomBookDetailsAppBar(),
           Padding(
-            padding: EdgeInsets.symmetric(horizontal: MediaQuery.of(context).size.width * 0.17),
+            padding: EdgeInsets.symmetric(
+                horizontal: MediaQuery.of(context).size.width * 0.2),
             child: const CustomBookImage(),
           ),
+          const SizedBox(
+            height: 43.0,
+          ),
+          const Text(
+            'The Jungle Book',
+            style: Styles.textStyle30,
+          ),
+          const SizedBox(
+            height: 6.0,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              'Rudyard Kipling',
+              style: Styles.textStyle18.copyWith(fontStyle: FontStyle.italic, fontWeight: FontWeight.w500),
+            ),
+          )
         ],
       ),
     );
   }
 }
-
