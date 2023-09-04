4e575aaa8f6aecde6b1136276d58a40f869017ce
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2023-09-04 23:01:39 +0300

    Create similar books list view

--- lib/features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view_body.dart
@@ -5,6 +5,7 @@ import 'package:my_bookley/features/home/presentation/views/widgets/custom_book_
 
 import 'books_action.dart';
 import 'custom_book_details_appbar.dart';
+import 'similar_books_list_view.dart';
 
 class BookDetailsViewBody extends StatelessWidget {
   const BookDetailsViewBody({super.key});
@@ -22,7 +23,7 @@ class BookDetailsViewBody extends StatelessWidget {
             child: const CustomBookImage(),
           ),
           const SizedBox(
-            height: 43.0,
+            height: 20.0,
           ),
           const Text(
             'The Jungle Book',
@@ -43,8 +44,24 @@ class BookDetailsViewBody extends StatelessWidget {
             height: 18.0,
           ),
           const BookRating(mainAxisAlignment: MainAxisAlignment.center),
-          const SizedBox(height: 37.0,),
+          const SizedBox(
+            height: 37.0,
+          ),
           const BooksAction(),
+          const SizedBox(
+            height: 30.0,
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              'You can also like',
+              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
+            ),
+          ),
+          const SizedBox(
+            height: 10.0,
+          ),
+          const SimilarBooksListView(),
         ],
       ),
     );
--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -11,7 +11,7 @@ class CustomBookImage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AspectRatio(
-      aspectRatio: 2.7 / 4,
+      aspectRatio: 2.6 / 4,
       child: Container(
         //height:  MediaQuery.of(context).size.height * .3,
         //width: MediaQuery.of(context).size.width * .25,
