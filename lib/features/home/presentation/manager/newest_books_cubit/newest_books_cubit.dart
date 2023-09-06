1b9581c0761eaf7d9751e56b0b0e38b84ef732af
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2023-09-06 21:27:23 +0300

    Newest books cubit done

--- lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart
@@ -1,10 +1,29 @@
 import 'package:bloc/bloc.dart';
 import 'package:meta/meta.dart';
+import 'package:my_bookley/features/home/data/repos/home_repo.dart';
 
 import '../../../data/models/book_model/BookModel.dart';
 
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchFeaturedBooks();
+    result.fold(
+      (failure) => {
+        emit(
+          NewestBooksFailure(failure.errMessage),
+        ),
+      },
+      (books) => {
+        emit(
+          NewestBooksSuccess(books),
+        ),
+      },
+    );
+  }
 }
