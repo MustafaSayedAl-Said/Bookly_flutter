6764d77f793dfe44becedf73d4dd60323217fc08
MustafaSayedAl-Said <mostafa_alsaid2011@hotmail.com>
2023-09-11 22:19:49 +0300

    cached network image

--- lib/features/home/presentation/views/widgets/custom_book_item.dart
+++ lib/features/home/presentation/views/widgets/custom_book_item.dart
@@ -1,27 +1,27 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-
-
 
 class CustomBookImage extends StatelessWidget {
   const CustomBookImage({
-    super.key, required this.imgURL,
+    super.key,
+    required this.imgURL,
   });
 
   final String imgURL;
   @override
   Widget build(BuildContext context) {
-    return AspectRatio(
-      aspectRatio: 2.6 / 4,
-      child: Container(
-        //height:  MediaQuery.of(context).size.height * .3,
-        //width: MediaQuery.of(context).size.width * .25,
-        decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(16),
-            image:  DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage(imgURL),
-            )),
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(16.0),
+      child: AspectRatio(
+        aspectRatio: 2.6 / 4,
+        child: CachedNetworkImage(
+          fit: BoxFit.fill,
+          // placeholder: (context, url) => const Center(
+          //   child: CircularProgressIndicator(),
+          // ),
+          imageUrl: imgURL,
+          errorWidget: (context, url, error) => const Icon(Icons.error),
+        ),
       ),
     );
   }
